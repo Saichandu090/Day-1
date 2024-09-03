dabfe624d9f90244db260813af709a350d100b9d
Saichandu090 <marrisaichandu143@gmail.com>
2024-09-03 15:01:08 +0530

    Adding Program3 to check two strings are equal or not

---
+++ src/Program3.java
@@ -0,0 +1,11 @@
+public class Program3
+{
+    String s1="Malayalam";
+    String s2="Malayalam";
+
+    public static void main(String[] args) {
+
+        Program3 p=new Program3();
+        System.out.println(p.s1.equals(p.s2));
+    }
+}
